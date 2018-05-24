f8828561132bd15fadddbaa8dd963c4e55cae8a1
Juliano Maia <leite.juliano@gmail.com>
2018-05-24 02:56:44 -0300

    change homepage to top tracks by country

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,19 +13,6 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
-    }
-
-    /**
-     * @Route("/last-fm/", name="last_fm")
-     * @param Request $request
      * @param LastFm $lastFm
      * @return \Symfony\Component\HttpFoundation\Response
      */
